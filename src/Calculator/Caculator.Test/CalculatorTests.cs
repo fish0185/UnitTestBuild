9ab5ce029ecbecb597d241c4d5dedee83f36aa48
Gary.Yu <Gary.Yu@zipmoney.com.au>
2017-06-06 18:51:04 +1000

    init

---
+++ src/Calculator/Caculator.Test/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caculator.Test
+{
+    using Calculator;
+
+    using NUnit;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void ShouldDoAdd()
+        {
+            var p = new Program();
+            var result = p.Add(10, 10);
+            Assert.That(result, Is.EqualTo(20));
+        }
+    }
+}
---
+++ src/Calculator/Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            var result = new Program().Add(10, 20);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
